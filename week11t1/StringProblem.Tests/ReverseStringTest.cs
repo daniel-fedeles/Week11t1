2dff25ce7bcf241d26cd8374b460c9978ff19f5d
danielfedeles <daniel.fedeles@tss-yonder.com>
2019-12-27 22:40:03 +0200

    StringProblem + Tests

---
+++ week11t1/StringProblem.Tests/ReverseStringTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringProblem.Tests
+{
+    [TestFixture]
+    public class ReverseStringTest
+    {
+        private StringReverse reverse;
+        [SetUp]
+        public void Setup()
+        {
+            reverse = new StringReverse();
+        }
+        [Test]
+        [Category("Valid Input")]
+        [TestCase("ab-cd", "dc-ba")]
+        [TestCase("a-bC-dEf-ghIj", "j-Ih-gfE-dCba")]
+        [TestCase("Test1ng-Leet=code-Q!", "Qedo1ct-eeLg=ntse-T!")]
+
+        public void Test_Valid_Input_String(string input, string expected)
+        {
+            //Arrange
+
+            //Act
+            string x = reverse.ReverseOnlyLetters(input);
+
+            //Assert
+            Assert.AreEqual(expected, x);
+
+        }
+
+        [Test]
+        [Category("Null Input")]
+        [TestCase(null, null)]
+        public void Test_Null_Input_String(string input, string expected)
+        {
+            //Arrange
+
+            //Act
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => reverse.ReverseOnlyLetters(input));
+
+        }
+        [Test]
+        [Category("Empty Input")]
+        [TestCase("", null)]
+        public void Test_Empty_Input_String(string input, string expected)
+        {
+            //Arrange
+
+            //Act
+            //Assert
+            Assert.Throws<ArgumentNullException>(() => reverse.ReverseOnlyLetters(input));
+
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
+            reverse = null;
+        }
+    }
+}
---
+++ week11t1/StringProblem/StringReverse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringProblem
+{
+    public class StringReverse
+    {
+        public string ReverseOnlyLetters(string S)
+        {
+            if (string.IsNullOrEmpty(S)) throw new ArgumentNullException(nameof(S));
+            S = S.Trim();
+            int start = 0;
+            int end = S.Length - 1;
+            char[] sCharArray = S.ToCharArray();
+
+            while (start < end)
+            {
+                if (Char.IsLetter(sCharArray[start]) && Char.IsLetter(sCharArray[end]))
+                {
+                    char temp = sCharArray[start];
+                    sCharArray[start] = sCharArray[end];
+                    sCharArray[end] = temp;
+                    start++;
+                    end--;
+                }
+                if (!Char.IsLetter(sCharArray[start]))
+                {
+                    start++;
+                }
+
+                if (!Char.IsLetter(sCharArray[end]))
+                {
+                    end--;
+                }
+            }
+
+            return new string(sCharArray);
+        }
+    }
+}
