dcac102d72a0467a8baac58c733be0813d660e3d
danielfedeles <daniel.fedeles@tss-yonder.com>
2019-12-28 01:20:43 +0200

    AddedGeneric Calculator with unit tests

---
+++ week11t1/GenericCalc.Tests/GenericCalcTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GenericCalc.Tests
+{
+    public class GenericCalcTest
+    {
+        private GenCalc<decimal> calcClass;
+        private GenCalc<string> strGenCalc;
+
+        public GenericCalcTest()
+        {
+            calcClass = new GenCalc<decimal>();
+            strGenCalc = new GenCalc<string>();
+        }
+
+
+        [Theory]
+        [InlineData(3, 7, 10)]
+        [InlineData(5, 9.2, 14.2)]
+        [InlineData(6.00, 6.3, 12.3)]
+        [InlineData(3, 0, 3)]
+        [InlineData(0, 9.2, 9.2)]
+        [InlineData(-5, 5, 0)]
+        [InlineData(-5, -5, -10)]
+        // [InlineData(5, -5, 0)]
+        [Trait("Category", "Add")]
+        public void TestAdd(decimal a, decimal b, decimal c)
+        {
+            Assert.Equal(c, calcClass.Add(a, b));
+
+        }
+
+        [Theory]
+        [InlineData("3", "7", "10")]
+        [Trait("Category", "Add")]
+        public void TestStrAdd(string a, string b, string c)
+        {
+            Assert.Throws<NotANumberException>(() => strGenCalc.Add(a, b));
+
+        }
+    }
+}
---
+++ week11t1/GenericCalc/GenCalc.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCalc
+{
+    public class GenCalc<T>
+    {
+
+        public T Add(T a, T b)
+        {
+            if (!EvaluateType(a, b)) throw new NotANumberException();
+            dynamic x = a;
+            dynamic y = b;
+            return x + y;
+        }
+        public T Sub(T a, T b)
+        {
+            if (!EvaluateType(a, b)) throw new NotANumberException();
+            dynamic x = a;
+            dynamic y = b;
+            return x - y;
+        }
+        public T Mul(T a, T b)
+        {
+            if (!EvaluateType(a, b)) throw new NotANumberException();
+            dynamic x = a;
+            dynamic y = b;
+            return x * y;
+        }
+        public T Div(T a, T b)
+        {
+            if (!EvaluateType(a, b)) throw new NotANumberException();
+            if (b == null)
+            { throw new ArgumentNullException(nameof(b));}
+            dynamic x = a;
+            dynamic y = b;
+            return x / y;
+        }
+
+        private bool EvaluateType(T a, T b)
+        {
+            if ((a is int || a is decimal || a is double || a is float)
+                || (b is int || b is decimal || b is double || b is float))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+    [Serializable]
+    public class NotANumberException : Exception
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public NotANumberException()
+        {
+        }
+
+        public NotANumberException(string message) : base(message)
+        {
+        }
+
+        public NotANumberException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        protected NotANumberException(
+            SerializationInfo info,
+            StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
